2e56a7289ad9a71a3ae13f6fdc8c6b8934366780
Nimap <Nimap@DESKTOP-RRJ6NCT>
2023-04-12 17:25:26 +0530

    Add project files.

---
+++ Day5/Properties2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    public class employee
+    {
+        public int EmpId { get; private set; }
+        public string EmpName { get; private set; }
+
+        public employee(int empid, string empname)
+        {
+            EmpId = empid;
+            EmpName = empname;
+        }
+    }
+
+    class Properties2
+    {
+        
+        public static void Main2()
+        {
+            employee e = new employee(101, "Tejas");
+            Console.WriteLine(e.EmpId);
+            Console.WriteLine(e.EmpName);
+
+        }
+    }
+}
